e10ea6bb917e72b9b85e794e1329ec3dedbd8a78
tawfiqsiam <toto.siam999@gmail.com>
2019-01-19 21:31:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.user.setActivity(argresult, {type:'LISTENING'});
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 } else 
 if (message.content.startsWith(PrEfix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/amira");
+  client.user.setGame(argresult, "https://www.twitch.tv/mrstar0");
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 }
 
